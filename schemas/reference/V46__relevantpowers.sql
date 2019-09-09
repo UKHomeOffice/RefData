b243b918bc0338d59f2421b95649bfcedfc09b6f
Mark Olliver <mark@digitalpatterns.io>
2019-09-09 14:54:33 +0100

    update merges

--- schemas/reference/V46__relevantpowers.sql
+++ schemas/reference/V46__relevantpowers.sql
@@ -4,6 +4,7 @@ CREATE TABLE relevantpowers (
   description TEXT NOT NULL,
   url VARCHAR(100),
   sop BOOLEAN NOT NULL,
+  legalgateway BOOLEAN NOT NULL,
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
@@ -16,6 +17,7 @@ COMMENT ON COLUMN relevantpowers.code IS '{"label": "Legal code", "description":
 COMMENT ON COLUMN relevantpowers.description IS '{"label": "Description", "description": "A short description of the power.", "summaryview": "true"}';
 COMMENT ON COLUMN relevantpowers.url IS '{"label": "URL", "description": "Link to the full power in legislation.", "summaryview": "true"}';
 COMMENT ON COLUMN relevantpowers.sop IS '{"label": "sop", "description": "Valid power for search of person.", "summaryview": "false"}';
+COMMENT ON COLUMN relevantpowers.legalgateway IS '{"label": "legalgateway", "description": "Is this a legal gateway?", "summaryview": "false"}';
 COMMENT ON COLUMN relevantpowers.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN relevantpowers.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
--- schemas/reference/V85__partnersystems.sql
+++ schemas/reference/V85__partnersystems.sql
@@ -1,15 +1,17 @@
 CREATE TABLE partnersystems (
   id INTEGER NOT NULL PRIMARY KEY,
-  name VARCHAR(40),
+  name VARCHAR(50),
+  shortcode VARCHAR(10),
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
 
 -- Table comment
-COMMENT ON TABLE partnersystems IS '{"label": "System Check", "description": "Checks performed against other computer systems.", "schemalastupdated": "07/08/2019", "dataversion": 1}';
+COMMENT ON TABLE partnersystems IS '{"label": "Partner System", "description": "Checks performed against other computer systems.", "schemalastupdated": "28/08/2019", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN partnersystems.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN partnersystems.name IS '{"label": "Name", "description": "The name of the computer systems checked.", "summaryview": "true"}';
+COMMENT ON COLUMN partnersystems.shortcode IS '{"label": "Short Code", "description": "Short code of the computer system.", "summaryview": "false"}';
 COMMENT ON COLUMN partnersystems.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN partnersystems.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
