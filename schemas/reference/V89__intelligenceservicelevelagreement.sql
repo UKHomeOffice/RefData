49e64f7d5304bfde520280c24d9dc5bf3f5711f9
Pedro Curado <4341960+p3dr0migue1@users.noreply.github.com>
2019-09-10 13:34:57 +0100

    Update V89__intelligenceservicelevelagreement.sql
    Fix missing double quotes

--- schemas/reference/V89__intelligenceservicelevelagreement.sql
+++ schemas/reference/V89__intelligenceservicelevelagreement.sql
@@ -11,7 +11,7 @@ COMMENT ON TABLE intelligenceservicelevelagreement IS '{"label": "Service Level 
 -- Column comments
 COMMENT ON COLUMN intelligenceservicelevelagreement.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN intelligenceservicelevelagreement.description IS '{"label": "Description", "description": "Description of the activity", "summaryview": "true"}';
-COMMENT ON COLUMN intelligenceservicelevelagreement.limitinhours IS '{"label": Time Limit", "description": "The maximum time this activity must be completed in.", "summaryview": "true"}';
+COMMENT ON COLUMN intelligenceservicelevelagreement.limitinhours IS '{"label": "Time Limit", "description": "The maximum time this activity must be completed in.", "summaryview": "true"}';
 COMMENT ON COLUMN intelligenceservicelevelagreement.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN intelligenceservicelevelagreement.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
