fcf32376db5602ca541590cc3bf08f3173e8d3e8
Mark Olliver <mark@digitalpatterns.io>
2019-10-24 10:21:39 +0100

    add items schema

---
+++ schemas/reference/V96__items.sql
@@ -0,0 +1,67 @@
+CREATE TABLE items (
+  id INT4 NOT NULL PRIMARY KEY,
+  name varchar(40) NOT NULL,
+  description varchar(100),
+  cites bool,
+  poao bool,
+  cbrn bool,
+  document bool,
+  tobacco bool,
+  alcohol bool,
+  weapons bool,
+  firearms bool,
+  ipr bool,
+  strategic bool,
+  avtc bool,
+  oils bool,
+  commodity bool,
+  good bool,
+  plant bool,
+  animal bool,
+  drugs bool,
+  financial bool,
+  indecent bool,
+  vehicle bool,
+  unitid int4 NOT NULL,
+  seizureqty numeric(5),
+  ienqty numeric(5),
+  validfrom TIMESTAMP WITH TIME ZONE,
+  validto TIMESTAMP WITH TIME ZONE
+);
+
+ -- Table comment
+COMMENT ON TABLE items IS '{"label": "Items", "description": "Items that cross the border", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+-- Column comments
+COMMENT ON COLUMN items.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
+COMMENT ON COLUMN items.name IS '{"label": "Name", "description": "Name of Item", "summaryview": "true"}';
+COMMENT ON COLUMN items.description IS '{"label": "Description", "description": "Description of item", "summaryview": "true"}';
+COMMENT ON COLUMN items.cites IS '{"label": "CITES", "description": "Is this a Convention on International Trade in Endangered Species of Wild Fauna and Flora Item?", "summaryview": "true"}';
+COMMENT ON COLUMN items.poao IS '{"label": "POAO", "description": "Is this a Product of Animal Origin Item?", "summaryview": "true"}';
+COMMENT ON COLUMN items.cbrn IS '{"label": "CBRN", "description": "Is this a Chemical, Biological, Radioactive or Nuclear Item?", "summaryview": "true"}';
+COMMENT ON COLUMN items.document IS '{"label": "Document", "description": "Is this a document?", "summaryview": "true"}';
+COMMENT ON COLUMN items.tobacco IS '{"label": "Tobacco", "description": "Is this tobacco?", "summaryview": "true"}';
+COMMENT ON COLUMN items.alcohol IS '{"label": "Alcohol", "description": "Is this alcohol?", "summaryview": "true"}';
+COMMENT ON COLUMN items.weapons IS '{"label": "Weapons", "description": "Is this a weapon?", "summaryview": "true"}';
+COMMENT ON COLUMN items.firearms IS '{"label": "Firearms", "description": "Is this a firearms?", "summaryview": "true"}';
+COMMENT ON COLUMN items.ipr IS '{"label": "IPR", "description": "Is this a international property rights Item?", "summaryview": "true"}';
+COMMENT ON COLUMN items.strategic IS '{"label": "Strategic", "description": "Is this on the UK Strategic Export Control List?", "summaryview": "true"}';
+COMMENT ON COLUMN items.oils IS '{"label": "Oils", "description": "Is this Vegetable/Petrochemical Oils?", "summaryview": "true"}';
+COMMENT ON COLUMN items.commodity IS '{"label": "Commodity", "description": "Is this a commodity Item?", "summaryview": "true"}';
+COMMENT ON COLUMN items.good IS '{"label": "Good", "description": "Is this a good?", "summaryview": "true"}';
+COMMENT ON COLUMN items.plant IS '{"label": "Plant", "description": "Is this a Plant?", "summaryview": "true"}';
+COMMENT ON COLUMN items.animal IS '{"label": "Animal", "description": "Is this a Animal?", "summaryview": "true"}';
+COMMENT ON COLUMN items.drugs IS '{"label": "Drugs", "description": "Is this a Drug?", "summaryview": "true"}';
+COMMENT ON COLUMN items.financial IS '{"label": "Financial", "description": "Is this a financial Item?", "summaryview": "true"}';
+COMMENT ON COLUMN items.indecent IS '{"label": "Indecent", "description": "Is this Indecent or obscene material?", "summaryview": "true"}';
+COMMENT ON COLUMN items.vehicle IS '{"label": "Vehicle", "description": "Is this a vehicle?", "summaryview": "true"}';
+COMMENT ON COLUMN items.avtc IS '{"label": "AVTC", "description": "Is this a Alternative Value Transfer Commodities?", "summaryview": "true"}';
+COMMENT ON COLUMN items.unitid IS '{"label": "Unit", "description": "Unit type of item", "summaryview": "true"}';
+COMMENT ON COLUMN items.seizureqty IS '{"label": "Seizure", "description": "Minimum quantity required for seizure?", "summaryview": "true"}';
+COMMENT ON COLUMN items.ienqty IS '{"label": "IEN", "description": "Minimum quantity required for seizure?", "summaryview": "true"}';
+COMMENT ON COLUMN items.validfrom IS '{"label": "Valid from date", "description": "items valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN items.validto IS '{"label": "Valid to date", "description": "items valid to date.", "summaryview" : "false"}';
+
+ -- GRANTs
+GRANT SELECT ON items TO ${anonuser};
+GRANT SELECT ON items TO ${serviceuser};
+GRANT SELECT ON items TO ${readonlyuser};
