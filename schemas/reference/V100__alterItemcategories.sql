6fab537117277c32f382b49a1d2016d1bfd15727
Belinda Cowey <belinda.cowey@digital.homeoffice.gov.uk>
2019-11-05 14:40:43 +0000

    CB-884 Remove itemlevels tables and correct spelling mistakes

---
+++ schemas/reference/V100__alterItemcategories.sql
@@ -0,0 +1,4 @@
+-- Table comment
+COMMENT ON TABLE itemcategories IS '{"label": "Item Categories", "description": "Categories used to identify items seized at the border.", "schemalastupdated": "24/10/2019", "dataversion": 1}';
+-- Column comment
+COMMENT ON COLUMN itemcategories.description IS '{"label": "Description", "description": "A short description of the item category.", "summaryview": "false"}';
