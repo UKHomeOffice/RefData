2b5873fe3a4a08b771eb6ff3f85f573622b51646
Ben Milnes <benjmilnes@outlook.com>
2019-11-08 16:51:25 +0000

    corrected description

--- schemas/reference/V95__pstmethods.sql
+++ schemas/reference/V95__pstmethods.sql
@@ -8,12 +8,12 @@ CREATE TABLE pstmethods (
 );
 
  -- Table comment
-COMMENT ON TABLE pstmethods IS '{"label": "Behaviour Signs", "description": "Behaviours Warning and Danger Signs", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE pstmethods IS '{"label": "PST Methodss", "description": "A list of PST methods", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN pstmethods.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN pstmethods.method IS '{"label": "Method", "description": "Technique method", "summaryview": "true"}';
 COMMENT ON COLUMN pstmethods.pressure IS '{"label": "Pressure", "description": "Is this a pressure method?", "summaryview": "true"}';
-COMMENT ON COLUMN pstmethods.nerve IS '{"label": "Nerve", "description": "Is this a nerve methods", "summaryview": "true"}';
+COMMENT ON COLUMN pstmethods.nerve IS '{"label": "Nerve", "description": "Is this a nerve method?", "summaryview": "true"}';
 COMMENT ON COLUMN pstmethods.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN pstmethods.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
