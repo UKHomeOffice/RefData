2ecbaadf2f14a45e9a948bdbc755842e8ccd634f
Ben Milnes <benjmilnes@outlook.com>
2019-12-03 11:31:50 +0000

    capitalized letters

--- schemas/reference/V101__occupation.sql
+++ schemas/reference/V101__occupation.sql
@@ -6,10 +6,10 @@ CREATE TABLE occupation (
 );
 
 -- Table comment
-COMMENT ON TABLE occupation IS '{"label": "occupation", "description": "A list of occupations as categorized by the Office for National Statistics", "schemalastupdated": "29/11/2019", "dataversion": 1}';
+COMMENT ON TABLE occupation IS '{"label": "Occupation", "description": "A list of occupations as categorized by the Office for National Statistics", "schemalastupdated": "29/11/2019", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN occupation.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
-COMMENT ON COLUMN occupation.occupation IS '{"label": "occupation", "description": "The name of the occupation.", "summaryview": "true"}';
+COMMENT ON COLUMN occupation.occupation IS '{"label": "Occupation", "description": "The name of the occupation.", "summaryview": "true"}';
 COMMENT ON COLUMN occupation.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN occupation.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
