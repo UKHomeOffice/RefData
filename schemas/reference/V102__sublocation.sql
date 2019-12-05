7db7f5933dffa8e90199e0f6da8b9943804c38d3
Ben Milnes <benjmilnes@outlook.com>
2019-12-05 14:38:40 +0000

    CB-1047

---
+++ schemas/reference/V102__sublocation.sql
@@ -0,0 +1,23 @@
+CREATE TABLE sublocation (
+  id INTEGER NOT NULL PRIMARY KEY,
+  portid INTEGER NOT NULL,
+  sublocation VARCHAR(30) NOT NULL,
+  description VARCHAR(70),
+  validfrom TIMESTAMP WITH TIME ZONE,
+  validto TIMESTAMP WITH TIME ZONE
+);
+
+-- Table comment
+COMMENT ON TABLE sublocation IS '{"label": "Sub Location", "description": "A list of Sub Locations of Ports", "schemalastupdated": "05/12/2019", "dataversion": 1}';
+-- Column comments
+COMMENT ON COLUMN sublocation.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
+COMMENT ON COLUMN sublocation.portid IS '{"label": "Port ID", "description": "ID of the Port that this Sub Location is part of", "summaryview": "false"}';
+COMMENT ON COLUMN sublocation.sublocation IS '{"label": "Sub Location", "description": "The name of the Sub Location.", "summaryview": "true"}';
+COMMENT ON COLUMN sublocation.description IS '{"label": "Description", "description": "A description of the Sub Location.", "summaryview": "true"}';
+COMMENT ON COLUMN sublocation.validfrom IS '{"label": "Valid from date", "description": "Sub Location valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN sublocation.validto IS '{"label": "Valid to date", "description": "Sub Location valid to date.", "summaryview" : "false"}';
+
+-- GRANTs
+GRANT SELECT ON sublocation TO ${anonuser};
+GRANT SELECT ON sublocation TO ${serviceuser};
+GRANT SELECT ON sublocation TO ${readonlyuser};
