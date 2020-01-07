3c773b91829f616def69b5efad914feb8909cf12
Mark Olliver <mark@digitalpatterns.io>
2020-01-07 10:12:44 +0000

    add control code

--- schemas/reference/V86__controlstrategy.sql
+++ schemas/reference/V86__controlstrategy.sql
@@ -2,6 +2,7 @@ CREATE TABLE controlstrategy (
   id int4 NOT NULL PRIMARY KEY,
   strategy VARCHAR(60) NOT NULL,
   priority VARCHAR(1) NOT NULL,
+  code VARCHAR(10) NOT NULL,
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
@@ -12,12 +13,10 @@ COMMENT ON TABLE controlstrategy IS '{"label": "Control strategy", "description"
 COMMENT ON COLUMN controlstrategy.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN controlstrategy.strategy IS '{"label": "Strategy", "description": "The strategy descriptor.", "summaryview": "true"}';
 COMMENT ON COLUMN controlstrategy.priority IS '{"label": "Priority", "description": "The priority letter assigned to the strategy.", "summaryview": "true"}';
+COMMENT ON COLUMN controlstrategy.code IS '{"label": "Code", "description": "Control Strategy Code.", "summaryview": "true"}';
 COMMENT ON COLUMN controlstrategy.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN controlstrategy.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
 -- GRANTs
 GRANT SELECT ON controlstrategy TO ${serviceuser};
 GRANT SELECT ON controlstrategy TO ${readonlyuser};
-
-ALTER TABLE itemlevel4
-  ADD CONSTRAINT controlstrategyitem4 FOREIGN KEY (controlstrategyid) REFERENCES controlstrategy(id);
