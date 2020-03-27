713011e363cd46bcac744316f5de53e611e76a49
Belinda Cowey <belinda.cowey@digital.homeoffice.gov.uk>
2020-03-27 14:19:50 +0000

    DATA-181 Remove negativeexam and noequipment columns from negative examination

--- schemas/reference/V103__negativeexamreasons.sql
+++ schemas/reference/V103__negativeexamreasons.sql
@@ -1,8 +1,6 @@
 CREATE TABLE negativeexamreasons (
     id INT4 NOT NULL PRIMARY KEY,
     name VARCHAR(100),
-    negativeexam BOOLEAN NOT NULL,
-    noequipment BOOLEAN NOT NULL,
     validfrom TIMESTAMP WITH TIME ZONE,
     validto TIMESTAMP WITH TIME ZONE
 );
@@ -12,8 +10,6 @@ COMMENT ON TABLE negativeexamreasons IS '{"label": "Reason the exam outcome was 
 -- Column comments
 COMMENT ON COLUMN negativeexamreasons.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN negativeexamreasons.name IS '{"label": "Reason for negative", "description": "List of possible reasons the exam outcome was negative", "summaryview": "true"}';
-COMMENT ON COLUMN negativeexamreasons.negativeexam IS '{"label": "Was this a negative exam", "description": "Was this a negative exam", "summaryview": "false"}';
-COMMENT ON COLUMN negativeexamreasons.noequipment IS '{"label": "Was this an equipment issue", "description": "Is this reason related to a lack of equipment", "summaryview": "false"}';
 COMMENT ON COLUMN negativeexamreasons.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN negativeexamreasons.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
