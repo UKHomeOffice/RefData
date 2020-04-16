429bb333eda2344d2e195d222a7ec20928bdd496
James Poulten <jpoulten@me.com>
2020-04-16 20:38:40 +0100

    New relations RefData Set

--- schemas/reference/V108__relation.sql
+++ schemas/reference/V108__relation.sql
@@ -1,17 +1,19 @@
 CREATE TABLE relation (
-  id INTERGER NOT NULL PRIMARY KEY, 
-  relation VARCHAR(20) NOT NULL, 
+  id INT4 NOT NULL PRIMARY KEY, 
+  relation VARCHAR(25) NOT NULL, 
   description VARCHAR(100), 
+  family BOOL, 
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
 
 -- Table comment 
-COMMENT ON TABLE realtion IS '{"label": "Relation", "description": "A list of relations to a specific party.", "schemalastupdated": "09/04/2020", "dataversion": 1}';
+COMMENT ON TABLE relation IS '{"label": "Relation", "description": "A list of relations to a specific party.", "schemalastupdated": "09/04/2020", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN relation.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
 COMMENT ON COLUMN relation.relation IS '{"label": "Relation", "description": "Name of relation type", "summaryview": "true"}';
 COMMENT ON COLUMN relation.description IS '{"label": "Description", "description": "Description of the relation", "summaryview": "true"}';
+COMMENT ON COLUMN relation.family IS '{"label": "Family", "description": "Is the relation a family member?", "summaryview": "false"}';
 COMMENT ON COLUMN relation.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN relation.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
