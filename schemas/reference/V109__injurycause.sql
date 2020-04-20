f318249eb2acff25e39bc8599ce1b1042d93e8cf
James Poulten <jpoulten@me.com>
2020-04-20 16:32:14 +0100

    Injury cause refData.

--- schemas/reference/V109__injurycause.sql
+++ schemas/reference/V109__injurycause.sql
@@ -1,20 +1,20 @@
 CREATE TABLE injurycause (
   id INTEGER NOT NULL PRIMARY KEY,
-  injurycause VARCHAR(25) NOT NULL,
+  injurycause VARCHAR(55) NOT NULL,
   description VARCHAR(100), 
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
 
 -- Table comment
-COMMENT ON TABLE injurycause is '{"label": "Injury Type", "description": "Different types of Injury.", "schemalastupdated": "17/04/2020", "dataversion": 1}';
+COMMENT ON TABLE injurycause IS '{"label": "Injury Type", "description": "Different types of Injury.", "schemalastupdated": "17/04/2020", "dataversion": 1}';
 -- Column comment
 COMMENT ON COLUMN injurycause.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
 COMMENT ON COLUMN injurycause.injurycause IS '{"label": "Cause of injury", "description": "Common causes of workplace injuries and accidents.", "summaryview": "true"}';
 COMMENT ON COLUMN injurycause.description IS '{"label": "Description", "description": "Description of the cause of injury.", "summaryview": "true"}';
 COMMENT ON COLUMN injurycause.validfrom IS '{"label": "Valid from date", "description": "Data valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN injurycause.validto IS '{"label": "Valid to date", "description": "Data valid to date.", "summaryview" : "false"}';
-Haha 
+
 -- GRANTs
 GRANT SELECT ON injurycause TO ${anonuser};
 GRANT SELECT ON injurycause TO ${serviceuser};
