ba352d53a43ba2a2d52d9954d420e9e3bf26c38b
Belinda Cowey <belinda.cowey@digital.homeoffice.gov.uk>
2020-06-03 10:52:03 +0100

    Fix spelling mistake

--- schemas/reference/V41__eventtype.sql
+++ schemas/reference/V41__eventtype.sql
@@ -7,7 +7,7 @@ CREATE TABLE eventtype (
 );
 
 -- Table comment
-COMMENT ON TABLE eventtype IS '{"label": "Event types", "description": "A list containing defferent event types.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE eventtype IS '{"label": "Event types", "description": "A list containing different event types.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN eventtype.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN eventtype.type IS '{"label": "Event types", "description": "Descriptions of the different event types.", "summaryview": "true"}';
