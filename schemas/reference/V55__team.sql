12d16ad45ac78d3524642e458456c1b47ad6eb5e
Belinda Cowey <Belinda@Mark-MacBook-Pro.local>
2020-06-18 10:11:44 +0100

    DATA-268 Intel team changes
    - Regional team flag
    - Team name changes
    - Expiration of Containers South team

--- schemas/reference/V55__team.sql
+++ schemas/reference/V55__team.sql
@@ -5,6 +5,7 @@ CREATE TABLE team (
   description TEXT,
   costcentrecode VARCHAR(50),
   parentteamid UUID REFERENCES team(id),
+  regionalteam BOOLEAN NOT NULL,
   bffunctiontypeid UUID NOT NULL REFERENCES bffunctiontypes(id),
   ministryid INT4 NOT NULL REFERENCES ministry(id),
   departmentid INT4 NOT NULL REFERENCES department(id),
@@ -17,7 +18,7 @@ CREATE TABLE team (
 );
 
 -- Table comment
-COMMENT ON TABLE team IS '{"label": "Teams", "description": "A list of border force teams.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE team IS '{"label": "Teams", "description": "A list of border force teams.", "schemalastupdated": "18/06/2020", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN team.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN team.name IS '{"label": "Name", "description": "The name of the team.", "summaryview": "true"}';
@@ -25,6 +26,7 @@ COMMENT ON COLUMN team.code IS '{"label": "Code", "description": "The team code.
 COMMENT ON COLUMN team.description IS '{"label": "Description", "description": "A description of the team.", "summaryview": "true"}';
 COMMENT ON COLUMN team.costcentrecode IS '{"label": "Cost centre", "description": "The teams cost centre code.", "summaryview": "true"}';
 COMMENT ON COLUMN team.parentteamid IS '{"label": "Parent Team ID", "description": "The unique ID of the parten team.", "summaryview": "false"}';
+COMMENT ON COLUMN team.regionalteam IS '{"label": "Regional Team", "description": "Regional team?", "summaryview": "true"}';
 COMMENT ON COLUMN team.bffunctiontypeid IS '{"label": "Function type ID", "description": "Link to border force functions entity.", "summaryview" : "false"}';
 COMMENT ON COLUMN team.ministryid IS '{"label": "Ministry ID", "description": "Link to ministry entity.", "summaryview" : "false"}';
 COMMENT ON COLUMN team.departmentid IS '{"label": "Department ID", "description": "Link to department entity.", "summaryview" : "false"}';
