8cb996934ff489749a11e3eb47010f9ea7a0058e
Ben Milnes <benjmilnes@outlook.com>
2020-12-09 15:42:31 +0000

    added comments to outcomes

--- schemas/reference/V83__outcomes.sql
+++ schemas/reference/V83__outcomes.sql
@@ -13,11 +13,14 @@ CREATE TABLE outcomes (
 );
 
 -- Table comment
-COMMENT ON TABLE outcomes IS '{"label": "outcomes", "description": "The outcome of an event.", "schemalastupdated": "03/10/2019", "dataversion": 1}';
+COMMENT ON TABLE outcomes IS '{"label": "outcomes", "description": "The outcome of an event.", "schemalastupdated": "03/12/2020", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN outcomes.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN outcomes.name IS '{"label": "Name", "description": "A description of the outcome.", "summaryview": "true"}';
 COMMENT ON COLUMN outcomes.nationalsecurity IS '{"label": "National Security", "description": "Is this outcome related to NS / CT?", "summaryview": "false"}';
+COMMENT ON COLUMN outcomes.is81outcome IS '{"label": "IS81 Outcome", "description": "Is this outcome related to an IS81?", "summaryview": "false"}';
+COMMENT ON COLUMN outcomes.is91feaoutcome IS '{"label": "IS91 FEA Outcome", "description": "Is this outcome related to an FEA?", "summaryview": "false"}';
+COMMENT ON COLUMN outcomes.is91custodianoutcome IS '{"label": "IS91 Custodian Outcome", "description": "Is this outcome related to an IS91?", "summaryview": "false"}';
 COMMENT ON COLUMN outcomes.remark IS '{"label": "Remark", "description": "Is this a remark?", "summaryview": "false"}';
 COMMENT ON COLUMN outcomes.nofurtheraction IS '{"label": "No Further Action", "description": "Is this an option for no further action", "summaryview": "false"}';
 COMMENT ON COLUMN outcomes.description IS '{"label": "Description", "description": "A definition of the outcome.", "summaryview": "true"}';
