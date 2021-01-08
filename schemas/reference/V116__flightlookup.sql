173ebb3b4130a90250e14747c362a44fcde595c0
Ivan G <aloneguid@outlook.com>
2021-01-08 12:08:07 +0000

    flight lookup table

---
+++ schemas/reference/V116__flightlookup.sql
@@ -0,0 +1,33 @@
+CREATE TABLE flightlookup (
+  id VARCHAR(20) NOT NULL PRIMARY KEY,
+  flightnumber VARCHAR(10) NOT NULL,
+  operator VARCHAR(50) NOT NULL,
+  departurecountryid INTEGER NOT NULL,
+  arrivalcountryid INTEGER NOT NULL,
+  departureairport VARCHAR(5) NOT NULL,
+  arrivalairport VARCHAR(5) NOT NULL,
+
+  validfrom TIMESTAMP WITH TIME ZONE,
+  validto TIMESTAMP WITH TIME ZONE,
+  updatedby VARCHAR(60) NULL
+);
+
+-- Table comment
+COMMENT ON TABLE flightlookup IS '{"label": "Flight Numbers", "description": "Scheduled flight numbers", "schemalastupdated": "08/01/2021", "dataversion": 1}';
+
+-- Column comments
+COMMENT ON COLUMN flightlookup.id IS '{"label": "Identifier", "description": "Unique identifying column", "summaryview": "false"}';
+COMMENT ON COLUMN flightlookup.flightnumber IS '{"label": "Flight Number", "description": "Flight number", "summaryview": "true"}';
+COMMENT ON COLUMN flightlookup.operator IS '{"label": "Flight Operator", "description": "Full name of flight operator", "summaryview": "true"}';
+COMMENT ON COLUMN flightlookup.departurecountryid IS '{"label": "Departure Country", "description": "ID of departure country (see country ref table).", "summaryview": "true"}';
+COMMENT ON COLUMN flightlookup.arrivalcountryid IS '{"label": "Arrival Country", "description": "ID of arrival country (see country ref table).", "summaryview": "true"}';
+COMMENT ON COLUMN flightlookup.departureairport IS '{"label": "Departure Airport IATA", "description": "Departure Airport IATA", "summaryview": "true"}';
+COMMENT ON COLUMN flightlookup.arrivalairport IS '{"label": "Arrival Airport IATA", "description": "Arrival Airport IATA", "summaryview": "true"}';
+
+COMMENT ON COLUMN flightlookup.validfrom IS '{"label": "Valid from date", "description": "Valid from date.", "summaryview": "false"}';
+COMMENT ON COLUMN flightlookup.validto IS '{"label": "Valid to date", "description": "Valid to date.", "summaryview": "false"}';
+COMMENT ON COLUMN flightlookup.updatedby IS '{"label": "Updated By", "description": "Record updated by", "summaryview": "false"}';
+
+-- GRANTs
+GRANT SELECT,INSERT,UPDATE ON flightlookup TO ${serviceuser};
+GRANT SELECT ON flightlookup TO ${readonlyuser};
