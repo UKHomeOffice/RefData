fee071b1c2a094f4d228833518faf7a24e01a776
Ivan G <aloneguid@outlook.com>
2021-01-13 14:28:43 +0000

    use standard password

--- python/sv.py
+++ python/sv.py
@@ -7,7 +7,7 @@ from psycopg2.errors import DuplicateDatabase
 import os
 
 # connect and create db
-conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="mypass")
+conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
 try:
