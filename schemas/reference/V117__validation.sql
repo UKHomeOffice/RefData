46ebbe1ee15c2a935463666f373b7f2e09aa1832
Ivan G <aloneguid@outlook.com>
2021-01-14 12:56:52 +0000

    fix the remaining errors

--- schemas/reference/V117__validation.sql
+++ schemas/reference/V117__validation.sql
@@ -14,3 +14,53 @@ COMMENT ON TABLE is81riskfactors IS '{"label": "Risk factors", "description": "R
 
 -- businessKey should be lowercase
 COMMENT ON COLUMN flightlookup.id IS '{"label": "Identifier", "businesskey": true, "description": "Unique identifying column, coposed from flight number and flight time.", "summaryview": "false"}';
+
+-- businesskey was missing
+COMMENT ON COLUMN is81riskfactors.id IS '{"label": "Identifier", "businesskey": true, "description": "Unique identifying column", "summaryview": "false"}';
+COMMENT ON COLUMN honotificationreasons.id IS '{"label": "Identifier", "description": "Unique identifying column", "summaryview": "false", "businesskey": true}';
+COMMENT ON COLUMN tdareasons.id IS '{"label": "Identifier", "description": "Unique identifying column", "summaryview": "false", "businesskey": true}';
+
+-- more than one businesskey, update comment to remove the rest
+COMMENT ON COLUMN currency.currency IS '{"label": "Currency", "description": "The name of the currency.", "summaryview": "true"}';
+COMMENT ON COLUMN department.name IS '{"label": "Department name", "description": "The name of the department.", "summaryview": "true"}';
+COMMENT ON COLUMN branch.name IS '{"label": "Name", "description": "The name of the branch or region.", "summaryview": "true", "aliases": "region"}';
+COMMENT ON COLUMN country.name IS '{"label": "Country name", "description": "The name of the country, territory or geographic area.", "summaryview": "true"}';
+COMMENT ON COLUMN carrierlist.name IS '{"label": "Carrier name", "description": "The name of the carrier.", "summaryview": "true"}';
+COMMENT ON COLUMN bflocationtype.description IS '{"label": "Description", "description": "Description of port crossing.", "summaryview": "true"}';
+COMMENT ON COLUMN icao.name IS '{"label": "Name", "description": "ICAO site name", "summaryview": "true"}';
+COMMENT ON COLUMN legtypes.description IS '{"label": "Description", "description": "A description of the journey type.", "summaryview": "true"}';
+COMMENT ON COLUMN targetmode.mode IS '{"label": "Target mode", "description": "The targeting mode type.", "summaryview": "true"}';
+COMMENT ON COLUMN unlocode.name IS '{"label": "Name", "description": "Location name", "summaryview": "true"}';
+COMMENT ON COLUMN workarea.area IS '{"label": "Area", "description": "A short description of the working area.", "summaryview": "true"}';
+COMMENT ON COLUMN activities.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false", "businesskey": true}';
+COMMENT ON COLUMN activities.activity IS '{"label": "Activity", "description": "A description of the activity.", "summaryview": "true"}';
+COMMENT ON COLUMN activities.activitytypeid IS '{"label": "Activity type ID", "description": "Link to activity type entity.", "summaryview" : "false"}';
+COMMENT ON COLUMN tdacodes.name IS '{"label": "Name", "description": "The name of the governing body.", "summaryview": "true"}';
+COMMENT ON COLUMN address.line1 IS '{"label": "Address line 1", "description": "First line of address.", "summaryview": "true"}';
+COMMENT ON COLUMN location.name IS '{"label": "Name", "description": "Location name", "summaryview": "true"}';
+COMMENT ON COLUMN technicalprojectstatus.status IS '{"label": "Status", "description": "The status of the project.", "summaryview": "true"}';
+COMMENT ON COLUMN division.name IS '{"label": "Name", "description": "The division name.", "summaryview": "true"}';
+COMMENT ON COLUMN command.name IS '{"label": "Name", "description": "The name of the command.", "summaryview": "true"}';
+COMMENT ON COLUMN team.name IS '{"label": "Name", "description": "The name of the team.", "summaryview": "true"}';
+COMMENT ON COLUMN technicalprojects.name IS '{"label": "Name", "description": "The name of the project.", "summaryview": "true"}';
+COMMENT ON COLUMN ferries.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false", "businesskey": true}';
+COMMENT ON COLUMN ferries.name IS '{"label": "Ferry name", "description": "The name of the ferry.", "summaryview": "true"}';
+COMMENT ON COLUMN ferries.carrierid IS '{"label": "Carrier ID", "description": "Link to the carrier list table by id.", "summaryview": "true"}';
+COMMENT ON COLUMN controlstrategy.strategy IS '{"label": "Strategy", "description": "The strategy descriptor.", "summaryview": "true"}';
+COMMENT ON COLUMN tdareasons.reason IS '{"label": "Reason", "description": "The reason for the request.", "summaryview": "true"}';
+COMMENT ON COLUMN maritimezone.name IS '{"label": "Name", "description": "The non-abbreviated name of the zone.", "summaryview": "true"}';
+COMMENT ON COLUMN eventclassification.label IS '{"label": "Label", "description": "The label for the breach type", "summaryview": "true"}';
+COMMENT ON COLUMN reasons.reason IS '{"label": "Description", "description": "The reason for the seizure.", "summaryview": "false"}';
+
+-- no JSON / not a valid JSON on column
+COMMENT ON COLUMN tdaapprovalstatus.updatedby IS '{"label": "Updated By", "description": "Record updated by", "summaryview": "false"}';
+COMMENT ON COLUMN eventclassification.id IS '{"label": "Identifier", "description": "Database unique identity record.", "businesskey": true, "summaryview": "false"}';
+
+
+
+
+
+
+
+
+
