823bde8f94712c9a2a4048ae026d2e9b6d19c7e6
Ivan G <aloneguid@outlook.com>
2021-01-18 16:55:49 +0000

    syntax errors

--- schemas/reference/V118__extstate.sql
+++ schemas/reference/V118__extstate.sql
@@ -8,15 +8,15 @@ CREATE TABLE _data_change_history (
 );
 
 -- Table comment
-COMMENT ON TABLE _data_change_history IS '{"label": "Data State", "owner": "cop@homeoffice.gov.uk", "description": "", "schemalastupdated": "08/01/2021", "dataversion": 1}';
+COMMENT ON TABLE _data_change_history IS '{"label": "Data State", "owner": "cop@homeoffice.gov.uk", "description": "Data State.", "schemalastupdated": "08/01/2021", "dataversion": 1}';
 
 -- Column comments
-COMMENT ON COLUMN _data_change_history.installedon IS '{"label": "Installation Date", "businessKey": true, "description": "Date of script installation.", "summaryview": "false"}';
+COMMENT ON COLUMN _data_change_history.installedon IS '{"label": "Installation Date", "businesskey": true, "description": "Date of script installation.", "summaryview": "false"}';
 COMMENT ON COLUMN _data_change_history.scriptname IS '{"label": "Script Name", "description": "Script name.", "summaryview": "false"}';
 
-COMMENT ON COLUMN flightlookup.validfrom IS '{"label": "Valid from date", "description": "Valid from date.", "summaryview": "false"}';
-COMMENT ON COLUMN flightlookup.validto IS '{"label": "Valid to date", "description": "Valid to date.", "summaryview": "false"}';
-COMMENT ON COLUMN flightlookup.updatedby IS '{"label": "Updated By", "description": "Record updated by", "summaryview": "false"}';
+COMMENT ON COLUMN _data_change_history.validfrom IS '{"label": "Valid from date", "description": "Valid from date.", "summaryview": "false"}';
+COMMENT ON COLUMN _data_change_history.validto IS '{"label": "Valid to date", "description": "Valid to date.", "summaryview": "false"}';
+COMMENT ON COLUMN _data_change_history.updatedby IS '{"label": "Updated By", "description": "Record updated by", "summaryview": "false"}';
 
 -- GRANTs
 GRANT SELECT,INSERT,UPDATE ON flightlookup TO ${serviceuser};
