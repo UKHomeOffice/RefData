a647e46501b982d552f29d8fb85c994573d79519
bethstephenson <BethStephenson@digital.homeoffice.gov.uk>
2021-04-28 11:25:55 +0100

    Added code column to crewrole for business key, removed duplicate business key from name column

---
+++ flyway/schemas/reference/V137__fix_crewrole.sql
@@ -0,0 +1,21 @@
+ALTER TABLE crewrole
+    ADD COLUMN code VARCHAR(5);
+
+-- Column comments
+COMMENT ON COLUMN crewrole.name IS '{"label": "Name", "description": "The name of the crew position.", "summaryview": "true"}';
+COMMENT ON COLUMN crewrole.code IS '{"label": "Code", "description": "A code for the crew position.", "summaryview": "true", "businesskey": "true"}';
+
+-- MATERIALIZED VIEW
+CREATE MATERIALIZED VIEW m_crewrole AS
+    SELECT * FROM crewrole
+    where (validfrom <= now()::date OR validfrom is null)
+      AND (validto >= now()::date + interval '1d' OR validto is null);
+
+CREATE UNIQUE INDEX ON m_crewrole (id);
+
+-- Apply Grant permissions
+GRANT SELECT,INSERT,UPDATE ON m_crewrole TO ${serviceuser};
+GRANT SELECT ON m_crewrole TO ${readonlyuser};
+
+-- Optional depending on dataset
+GRANT SELECT ON m_crewrole TO ${anonuser};
